df1b3f9620385ec0e010598660ec61cf1ed83628
PeiyunBai <2483388021@qq.com>
2022-02-24 19:51:32 +0800

    feat: 修复本地启动

--- js-code/build/webpack.dev.js
+++ js-code/build/webpack.dev.js
@@ -1,21 +1,30 @@
-const webpack = require('webpack')
-const { merge } = require('webpack-merge')
-const webpackCommonConf = require('./webpack.common.js')
-const { distPath } = require('./paths.js')
+/*
+ * @Author: baipeiyun
+ * @Date: 2022-02-14 15:11:11
+ * @LastEditors: baipeiyun
+ * @LastEditTime: 2022-02-24 19:48:49
+ * @FilePath: /临时用/fe-interview-100/js-code/build/webpack.dev.js
+ * @Description:
+ */
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const webpackCommonConf = require("./webpack.common.js");
+const { distPath } = require("./paths.js");
 
 module.exports = merge(webpackCommonConf, {
-    mode: 'development',
+    mode: "development",
     plugins: [
         new webpack.DefinePlugin({
-            ENV: JSON.stringify('development')
-        })
+            ENV: JSON.stringify("development"),
+        }),
     ],
     devServer: {
+        host: "127.0.0.1",
         port: 3000,
-        progress: true,  // 显示打包的进度条
-        contentBase: distPath,  // 根目录
-        // open: true,  // 自动打开浏览器
-        compress: true  // 启动 gzip 压缩
+        progress: true, // 显示打包的进度条
+        contentBase: distPath, // 根目录
+        open: true, // 自动打开浏览器
+        compress: true, // 启动 gzip 压缩
     },
-    devtool: 'eval-cheap-source-map'
-})
+    devtool: "eval-cheap-source-map",
+});
--- js-code/src/my-code/1.ts
+++ js-code/src/my-code/1.ts
@@ -2,7 +2,7 @@
  * @Author: baipeiyun
  * @Date: 2022-02-22 20:15:05
  * @LastEditors: baipeiyun
- * @LastEditTime: 2022-02-22 20:43:31
+ * @LastEditTime: 2022-02-24 19:30:54
  * @FilePath: /临时用/fe-interview-100/js-code/src/my-code/1.ts
  * @Description:
  */
@@ -13,16 +13,18 @@ function retate(arr:number[],k:number):number[]{
     if(!arr.length|| k===0) return arr;
     const step = Math.abs(k % arr.length);
 
-    // for(let i=1; i<=step;i++){
-    //     const a = arr.pop();
-    //     if(a!=null){
-    //         arr.unshift(a)
-    //     }
-    // }
-
-    const part1 = arr.splice(-step);
-    // const
-
+    for(let i=1; i<=step;i++){
+        const a = arr.pop();
+        if(a!=null){
+            arr.unshift(a)
+        }
+    }
     return arr;
+
+//     const part1 = arr.splice(-step);
+// const part2 = arr.splice(0,length-step);
+// const part3 = part1.concat(part2);
+// return part3
 }
+
 
